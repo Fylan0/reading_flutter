766cf7d4cbd02000dac05f9ab1124b9514656200
FengChen <fengjiaxin01@corp.netease.com>
2024-02-07 21:16:44 +0800

    [ADD] Android提供battery-getBatteryLevel方法

--- android/app/src/main/kotlin/com/fylan/reading_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/fylan/reading_flutter/MainActivity.kt
@@ -1,6 +1,17 @@
 package com.fylan.reading_flutter
 
+import com.fylan.reading_flutter.flutter.federated_plugin.FederatedPlugin
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
+
+class MainActivity : FlutterActivity() {
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+
+        // 在 FlutterEngine 中注册你的插件
+        flutterEngine.plugins.add(FederatedPlugin())
+    }
 }
